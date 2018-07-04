9e56f36e5f8b49ebbc2422df793ceddc1f472148
wangwei <40561381+wangwei0626@users.noreply.github.com>
2018-07-04 15:31:51 +0800

    Add files via upload

---
+++ Thread/RunnableDemo.java
@@ -0,0 +1,35 @@
+//实现Runnable实现多线程
+/**建议使用线程的时候可以继承接口，Runnable适合相同的代码资源处理同一个资源*/
+package Thread;
+
+public class RunnableDemo implements Runnable {
+    private String name;
+    
+	public RunnableDemo(String name) {
+		
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int i = 1; i < 5; i++) {
+			
+			System.out.println(name + "运行" + i);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//创建线程对象
+		RunnableDemo R1 = new RunnableDemo("A");
+		//继承接口的线程类需要通过创建Thread实例执行
+		Thread D1 = new Thread(R1);
+		RunnableDemo R2 = new RunnableDemo("B");
+		Thread D2 = new Thread(R2);
+		D2.start();
+		D1.start();
+	}
+
+}
---
+++ Thread/ThreadDemo.java
@@ -0,0 +1,33 @@
+//继承Thread类实现多线程，实质Thread也是实现的Runnable接口
+package Thread;
+
+import java.util.Iterator;
+
+public class ThreadDemo extends Thread {
+    private String name;
+    
+	public ThreadDemo(String name) {
+		this.name = name;
+	}
+	public void run() {
+		for(int i = 0;i < 5;i++) {
+			try {
+				//线程的休眠，每间隔2秒钟执行一次线程
+				Thread.sleep(2000);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			System.out.println(name + "运行" + i);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//创建线程对象
+		ThreadDemo T1 = new ThreadDemo("test1");
+		ThreadDemo T2 = new ThreadDemo("test2");
+		//执行start方法，获取CPU资源之后会自动 执行run方法。
+		T1.start();
+		T2.start();
+	}
+
+}
